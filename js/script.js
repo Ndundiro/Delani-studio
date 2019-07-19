ccb58e8021a7d66361f6ca6f460424423eddbe56
Ndundiro_Kamau <ndundirokamau@gmail.com>
2019-07-19 18:11:45 +0300

    Adds the jquery function that enables toggle between the what we do icons and description.

---
+++ js/script.js
@@ -0,0 +1,21 @@
+// what we do section
+$(document).ready(function(){
+    $(".design,#hide1").click(function(){
+        $(".design").toggle();
+        $("#hide1").toggle();
+    });
+    $(".development,#hide2").click(function(){
+        $(".development").toggle();
+        $("#hide2").toggle();
+    });
+    $(".product,#hide3").click(function(){
+        $(".product").toggle();
+        $("#hide3").toggle();
+    })
+
+
+
+
+
+
+});
